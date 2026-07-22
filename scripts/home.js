a33e161de5e5530e53534061aef251b2dfe00886
viin <vynn@quantumtrip.org>
2026-07-22 01:55:47 -0400

    Expanding circles

--- scripts/home.js
+++ scripts/home.js
@@ -19,11 +19,40 @@ class Home {
     this.canvas = document.getElementById("homeCanvas");
     this.ctx = this.canvas.getContext("2d");
     this.brush = new CanvasTools(this.canvas, this.ctx);
+    this.drawLoop = false;
+    this.drawX = 0;
+    this.drawY = 0;
+    this.circleSize = 10;
+  }
+
+  setPos(ev) {
+    this.drawX = ev.clientX;
+    this.drawY = ev.clientY;
+  }
+
+  drawShape() { 
+    if (this.drawLoop) {
+      this.brush.drawCircle(this.drawX, this.drawY, this.circleSize);
+      this.circleSize += 5;
+      setTimeout(() => this.drawShape(), 75);
+    }
   }
 
   listen() {
-    this.canvas.onclick = (ev) => {
-      this.brush.drawCircle(ev.clientX, ev.clientY, 10)
+    this.canvas.onmouseup = (ev) => {
+      this.drawLoop = false;
+    }
+
+    this.canvas.onmousemove = (ev) => {   
+      this.setPos(ev);
+
+      if(!this.drawLoop && ev.buttons == 1) {
+        this.circleSize = 10;
+        this.drawLoop = true;
+        this.drawShape();
+      }
+
+      this.drawLoop = (ev.buttons == 1);      
     }
   }
 }
